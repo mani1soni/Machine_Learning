aca63e440db90366c520b0184f5b0b43e41ef5b0
Manish Soni <manish.soni8403@gmail.com>
2018-01-20 15:24:57 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.5
+
+from sklearn import tree
+
+#apple_smooth = 0 and orange_bumpy = 1
+
+#features of fruit --  [weight,texture]
+features=[[130,0],[120,0],[110,0],[100,0],[140,0],[145,1],[150,1],[160,1],[170,1],[180,1]]
+
+labels=[0,0,0,0,0,1,1,1,1,1,1]
+
+#calling DecisionTreeClassifier function
+
+clf=tree.DecisionTreeClassifier()
+output=clf.fit[features,labels]
+
+#testing data
+print (output.predict[[130,0]])
+
+
